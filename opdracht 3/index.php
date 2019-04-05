5dfa18db399c9c7bc826104544f45f57b3aca99a
Xx-Darkness-xX02 <kjell.roelofs@student.rocvf.nl>
2019-04-05 09:37:20 +0200

    opdracht 3

---
+++ opdracht 3/index.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kroel
+ * Date: 5-4-2019
+ * Time: 09:03
+ */?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Opdracht 3</title>
+</head>
+<body>
+<?php
+$begroeting = 'hallo ';
+$ik = 'ik ';
+$ben = 'ben ';
+$naam = 'kjell ';
+$vraag = 'hoe ';
+$a = 6;
+$b = 4;
+if($a > $b)
+    echo 'echo ' . $begroeting . $ik . $ben . $naam . $vraag . 'gaat het?';
+
+?>
+</body>
+</html>
